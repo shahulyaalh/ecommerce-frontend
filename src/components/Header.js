e2c7414f4f28e7aa85b4e780ea076033f67831bf
Shahul Hameed <146325988+shahulyaalh@users.noreply.github.com>
2024-12-09 13:40:49 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header({cartItems=[]}){
     <div className="col-12 col-md-3">
       <div className="navbar-brand">
         <Link to={'/'}>
-        <img width="120px" src=" /images/logo-1.png" />
+        <img width="120px" src=" /images/logo1.png" />
         </Link>
       </div>
     </div>
